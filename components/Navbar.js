82930d748e6476e522e083db6b6642c67bc62b63
Iqbal <iqbalhossen1418@gmail.com>
2023-12-05 23:25:09 +0600

    Home page start..12/5/23

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from '../styles/navbar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -6,6 +6,7 @@ import { FaBars } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { FaWindowClose } from "react-icons/fa";
 const Navbar = () => {
+  const [searchdrop, setsearchdrop] = useState(false)
   return (
     <>
       <div className={Styles.navbar}>
@@ -15,17 +16,21 @@ const Navbar = () => {
         </div>
         <div className={Styles.navlist}>
           <Link href='/signup'><span>Top Trending</span></Link>
-          <Link href='/'><span>Bset Picks</span></Link>
-          <Link href='/'><span>Smart Devices</span></Link>
-          <Link href='/'><span>Gadgets</span></Link>
-          <Link href='/'><span>News</span></Link>
-          <Link href='/'><span>Innovations</span></Link>
-          <Link href='/'><span>Category</span></Link>
+          <Link href='/Allusers'><span>Bset Picks</span></Link>
+          <Link href='/smartdevices'><span>Smart Devices</span></Link>
+          <Link href='/gadgets'><span>Gadgets</span></Link>
+          <Link href='/news'><span>News</span></Link>
+          <Link href='/innovations'><span>Innovations</span></Link>
+          <Link href='/category'><span>Category</span></Link>
         </div>
 
         <div className={Styles.navright}>
           <label htmlFor='nocheck'>
-            <div className={Styles.searchbar}><IoIosSearch /></div>
+            <div onMouseOver={()=>{setsearchdrop(true)}} onMouseLeave={()=>{setsearchdrop(false)}} className={Styles.searchbar}><IoIosSearch /></div>
+            <div onMouseOver={()=>{setsearchdrop(true)}} onMouseLeave={()=>{setsearchdrop(false)}}  className={searchdrop === false ? Styles.searchinputone:Styles.searchinput}>
+                <input  type='text' placeholder='search here'/>
+                <p><IoIosSearch/></p>
+              </div>
           </label>
           <input type='checkbox' id={Styles.check} />
           <label htmlFor={Styles.check}>
@@ -43,13 +48,13 @@ const Navbar = () => {
               </div>
             </div>
             <div className={Styles.sidelist}>
-              <Link href='/'><p>Top Trending</p></Link>
-              <Link href='/signup'><p>Bset Picks</p></Link>
-              <Link href='/signup'><p>Smart Devices</p></Link>
-              <Link href='/about'><p>Gadgets</p></Link>
-              <Link href='/signup'><p>News</p></Link>
-              <Link href='/contact'><p>Innovations</p></Link>
-              <Link href='/signup'><p>Category</p></Link>
+              <Link href='/signup'><p>Top Trending</p></Link>
+              <Link href='/Allusers'><p>Bset Picks</p></Link>
+              <Link href='/smartdevices'><p>Smart Devices</p></Link>
+              <Link href='/gadgets'><p>Gadgets</p></Link>
+              <Link href='/news'><p>News</p></Link>
+              <Link href='/innovations'><p>Innovations</p></Link>
+              <Link href='/category'><p>Category</p></Link>
             </div>
 
           </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,86 @@
 
+import Image from 'next/image';
 import Styles from '../styles/index.module.css';
+import Link from 'next/link';
 
 const Home = () => {
   return (
-    <>  
-     <div>
-         Home
+    <>
+      <div className={Styles.container}>
+        <Link href={'/'} className={Styles.trend1}>
+          <div className={Styles.trendtext} >
+            <h2>Smart Watches Unveiled</h2>
+            <h5>Join us in exploring the smart revolution on your wrist!</h5>
+          </div>
+          <Image className={Styles.trendimg} height={3743} width={5614} src='/watchtrend1.jpg' alt='watch' />
+        </Link>
+        <div className={Styles.trend2}>
+          <Link href={'/'} className={Styles.trend1}>
+            <div className={Styles.trendtext} >
+              <h2>Smart Watches Unveiled</h2>
+              <h5>Join us in exploring the smart revolution on your wrist!</h5>
+            </div>
+            <Image className={Styles.trendimg} height={3743} width={5614} src='/watchtrend1.jpg' alt='watch' />
+          </Link>
+        </div>
+        <div className={Styles.trend3}>
+          <Link href={'/'} className={Styles.trend1}>
+            <div className={Styles.trendtext} >
+              <h2>Smart Watches Unveiled</h2>
+              <h5>Join us in exploring the smart revolution on your wrist!</h5>
+            </div>
+            <Image className={Styles.trendimg} height={3743} width={5614} src='/watchtrend1.jpg' alt='watch' />
+          </Link>
+        </div>
+        <div className={Styles.trend4}>
+          <Link href={'/'} className={Styles.trend1}>
+            <div className={Styles.trendtext} >
+              <h2>Smart Watches Unveiled</h2>
+              <h5>Join us in exploring the smart revolution on your wrist!</h5>
+            </div>
+            <Image className={Styles.trendimg} height={3743} width={5614} src='/watchtrend1.jpg' alt='watch' />
+          </Link>
+        </div>
+      </div>
+      <div className={Styles.blogcontainer}>
+        <div className={Styles.blogs}>
+
+          <div className={Styles.blogItem}>
+            <div className={Styles.blogimg}>
+
+            </div>
+            <div className={Styles.aboutblog}>
+
+            </div>
+          </div>
+          <div className={Styles.blogItem}>
+            <div className={Styles.blogimg}>
+
+            </div>
+            <div className={Styles.aboutblog}>
+
+            </div>
+          </div>
+          <div className={Styles.blogItem}>
+            <div className={Styles.blogimg}>
+
+            </div>
+            <div className={Styles.aboutblog}>
+
+            </div>
+          </div>
+          <div className={Styles.blogItem}>
+            <div className={Styles.blogimg}>
+
+            </div>
+            <div className={Styles.aboutblog}>
+
+            </div>
+          </div>
+        </div>
+        <div className={Styles.bloghilight}>
+
+        </div>
       </div>
     </>
   );
